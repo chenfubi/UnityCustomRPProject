e6986a61597953900575f64cfc9ed4f773202ad8
chenfubi <cfb877@sina.com>
2020-12-28 18:45:01 +0800

    draw call

---
+++ Assets/Scripts/DrawCall/PerObjectMaterialProperties.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static MaterialPropertyBlock block;
+    [SerializeField]
+    Color baseColor = Color.white;
+    private void OnValidate() {
+        if (block == null) {
+            block = new MaterialPropertyBlock();
+        }
+        block.SetColor(baseColorId, baseColor);
+        GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        OnValidate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
