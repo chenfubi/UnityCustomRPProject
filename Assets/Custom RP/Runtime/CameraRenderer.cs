103e95a7826ca32fa555522ea71ff0e52ec93551
chenfubi <cfb877@sina.com>
2021-01-07 08:31:45 +0800

    baked light

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -57,7 +57,8 @@ public partial class CameraRenderer
             unlitShaderTagId, sortingSettings
         ) {
             enableDynamicBatching = useDynamicBatching,
-            enableInstancing = useGPUInstancing
+            enableInstancing = useGPUInstancing,
+            perObjectData = PerObjectData.Lightmaps | PerObjectData.LightProbe
         };
         drawingSettings.SetShaderPassName(1, LitShaderTagId);
         var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
